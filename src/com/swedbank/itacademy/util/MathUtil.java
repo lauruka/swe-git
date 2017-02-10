ab4cda56a668b38f568f6cd6ad2c458abf6429d2
Laurute <laraa3@gmail.com>
2017-02-10 17:46:26 +0200

    Inserted factorial method

--- src/com/swedbank/itacademy/util/MathUtil.java
+++ src/com/swedbank/itacademy/util/MathUtil.java
@@ -3,7 +3,12 @@ package com.swedbank.itacademy.util;
 public class MathUtil {
 
     // TODO implement code
-    public static int factorial() {
-        return 0;
+    public static int factorial(int number) {
+        int factorial = 1;
+        while (number > 1) {
+            factorial = factorial * number;
+            number--;
+        }
+        return factorial;
     }
 }
